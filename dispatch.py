4bd8deb2fb2fa0c05ed383555436cf8cf1e40208
Kippiii <ianorzel@gmail.com>
2022-11-23 18:47:43 -0500

    Adding basic format for dispatching to stack overflow exploits

--- dispatch.py
+++ dispatch.py
@@ -2,11 +2,45 @@
 Determines which exploits to call
 """
 
-def dispatch_exploits() -> None:
+import re
+from pwn import ELF, process
+
+pointer_re = r"""0x[0-9a-f]*"""
+
+def dispatch_exploits(file_path: str) -> None:
     """
     Calls exploits based on possible ones
     """
+    syms = ELF(file_path).symbols.keys()
 
-    # Ian (Stack)
+    # Get all vulnerabilites
+    vulns = ?
+
+    if has_stack_overflow(vulns):
+        # ret2win
+        if 'win' in syms:
+            pass
+
+        # ret2system
+        if 'system' in syms:
+            pass
+
+        # ret2execve
+        if 'execve' in syms:
+            pass
+
+        # ret2syscall
+        if 'syscall' in syms:
+            pass
+
+        # ret2libc
+        output = process(file_path).recv()
+        comp = re.compile(pointer_re)
+        if comp.match(output) is not None:
+            pass
+
+        # ropwrite
+        if 'pwnme' in syms:
+            pass
 
     # Nick (Format)
