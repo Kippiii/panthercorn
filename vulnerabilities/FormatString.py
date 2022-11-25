f4268c39a4f655aeaa922972af273b4a72a6d43b
Kippiii <ianorzel@gmail.com>
2022-11-25 16:34:23 -0500

    Adding in basic structure for vulnerabilities

--- vulnerabilities/FormatString.py
+++ vulnerabilities/FormatString.py
@@ -2,4 +2,12 @@
 Deals with finding format string attack vulnerabilites
 """
 
+from typing import List
+
 # Ash
+
+class FormatString:
+    pass # TODO
+
+def get_format_string_vulns() -> List[FormatString]:
+    return [] # TODO
--- vulnerabilities/StackOverflow.py
+++ vulnerabilities/StackOverflow.py
@@ -2,4 +2,12 @@
 Deals with finding stack overflow vulnerabilites
 """
 
+from typing import List
+
 # Carlos
+
+class StackOverflow:
+    pass # TODO
+
+def get_stack_overflow_vulns() -> List[StackOverflow]:
+    return [] # TODO
