5f283433fe956749d860429e240a4ef91f21d629
Kippiii <ianorzel@gmail.com>
2022-11-25 16:42:01 -0500

    Updating stack dispatching to use real values

--- dispatch.py
+++ dispatch.py
@@ -4,8 +4,16 @@ Determines which exploits to call
 
 import re
 from pwn import ELF, process
+from typing import List, Union
+
+from vulnerabilities import FormatString, StackOverflow, get_format_string_vulns, get_stack_overflow_vulns
+from exploits import ret2win, ret2system, ret2execve, ret2syscall, ret2libc, ropwrite
 
 pointer_re = r"""0x[0-9a-f]*"""
+
+def end_prog(flag: str) -> None:
+    print(f"Flag is {flag}")
+    exit(0)
 
 def dispatch_exploits(file_path: str) -> None:
     """
@@ -14,33 +22,46 @@ def dispatch_exploits(file_path: str) -> None:
     syms = ELF(file_path).symbols.keys()
 
     # Get all vulnerabilites
-    vulns = ?
+    vulns: List[Union[FormatString, StackOverflow]] = get_format_string_vulns() + get_stack_overflow_vulns()
 
-    if has_stack_overflow(vulns):
-        # ret2win
-        if 'win' in syms:
-            pass
+    for vuln in vulns:
+        if isinstance(vuln, StackOverflow):
+            # ret2win
+            if 'win' in syms:
+                flag = ret2win(vuln)
+                if flag is not None:
+                    end_prog(flag)
 
-        # ret2system
-        if 'system' in syms:
-            pass
+            # ret2system
+            if 'system' in syms:
+                flag = ret2system(vuln)
+                if flag is not None:
+                    end_prog(flag)
 
-        # ret2execve
-        if 'execve' in syms:
-            pass
+            # ret2execve
+            if 'execve' in syms:
+                flag = ret2execve(vuln)
+                if flag is not None:
+                    end_prog(flag)
 
-        # ret2syscall
-        if 'syscall' in syms:
-            pass
+            # ret2syscall
+            if 'syscall' in syms:
+                flag = ret2syscall(vuln)
+                if flag is not None:
+                    end_prog(flag)
 
-        # ret2libc
-        output = process(file_path).recv()
-        comp = re.compile(pointer_re)
-        if comp.match(output) is not None:
-            pass
+            # ret2libc
+            output = process(file_path).recv()
+            comp = re.compile(pointer_re)
+            if comp.match(output) is not None:
+                flag = ret2libc(vuln)
+                if flag is not None:
+                    end_prog(flag)
 
-        # ropwrite
-        if 'pwnme' in syms:
-            pass
+            # ropwrite
+            if 'pwnme' in syms:
+                flag = ropwrite(vuln)
+                if flag is not None:
+                    end_prog(flag)
 
-    # Nick (Format)
+        # Nick (Format)
