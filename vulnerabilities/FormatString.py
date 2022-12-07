2a441965cb42e691c28ec75035957a58fcd8a94d
Kippiii <ianorzel@gmail.com>
2022-12-07 15:23:31 -0500

    Adding in format string vulnerability

--- vulnerabilities/FormatString.py
+++ vulnerabilities/FormatString.py
@@ -3,13 +3,33 @@ Deals with finding format string attack vulnerabilites
 """
 
 from typing import List
+import re
+from pwn import *
 
 
-# Ash
+POINTER_RE = r"""0x([0-9A-F]+)"""
+
 
 class FormatString:
-    pass  # TODO
+    payload_start: bytes
+
+    def __init__(self, payload_start: bytes) -> None:
+        self.payload_start = payload_start
 
 
-def get_format_string_vulns() -> List[FormatString]:
-    return []  # TODO
+def get_format_string_vulns(bin_path: str) -> List[FormatString]:
+    vulns = []
+    p = process(bin_path)
+    num_payloads = 0
+    while p.poll() is None:
+        if p.can_recieve(timeout=1):
+            out = p.recvS()
+            comp = re.compile(POINTER_RE)
+            srch = re.search(out)
+            if srch:
+                vulns.append(FormatString(b"%p.%p.%p\n" * (num_payloads - 1)))
+        else:
+            num_payloads += 1
+            p.send(b"%p.%p.%p\n")
+
+    return vulns
